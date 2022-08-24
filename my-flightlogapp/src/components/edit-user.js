bce541bdc21274f7f4224db274817c03d1e44681
Kay Edwards <kaylaedwards575@gmail.com>
2022-08-24 14:04:48 -0400

    front-end login page

---
+++ my-flightlogapp/src/components/edit-user.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Edituser extends Component{
+    render(){
+
+        return (
+            <div>
+                <p> You are on the edit users component </p>
+
+            </div>    
+        )
+    }
+}
---
+++ my-flightlogapp/src/components/user-list.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class Userlist extends Component{
+    render(){
+
+        return (
+            <div>
+                <p> You are on the list users component </p>
+
+            </div>    
+        )
+    }
+}
